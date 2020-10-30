b3cddf0cc5a84625683c853bb0d1d14430d1acf2
Z.Cao <z.cao@zju.edu.cn>
2020-10-30 16:21:32 +0800

    Fix Solution#10

--- 010.py
+++ 010.py
@@ -1,14 +1,29 @@
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        if not nums:
-            return []
-        res = []
-        len_nums = len(nums)
-        nums.sort()
-        def dfs(re,index):
-            if re not in res:
-                res.append(re)
-            for i in range(index,len_nums):
-                dfs(re+[nums[i]],i+1)
-        dfs([],0)
-        return res
+    def isMatch(self, s: str, p: str) -> bool:
+        dp=[[False for j in range(len(p)+1)] for i in range(len(s)+1)]    #生成dp矩阵，给定初始值False
+        dp[0][0]=True
+        for k in range(1,len(p)+1):                 #初始条件：dp[0][k] k∈[1,2,...,len(p)] 即二维数组第一行
+            if len(p)>1:
+                if p[k-1]=="*":
+                    dp[0][k]=dp[0][k-2]
+                else:
+                    dp[0][k]=False
+            else:
+                dp[0][k]=False
+        for l in range(1,len(s)+1):                 #初始条件 dp[l][0] l∈[1,2,...,len(p)] 即二维数组第一列
+            dp[l][0]=False
+
+        for i in range(1,len(s)+1):                 #填写二维数组列表内容
+            for j in range(1,len(p)+1):
+                if s[i-1]==p[j-1] or p[j-1]==".":               #常规情况
+                    dp[i][j]=dp[i-1][j-1]
+                elif p[j-1]=="*":                               #处理带*情况
+                    if dp[i][j-2]==True:
+                        dp[i][j]=True
+                    elif (p[j-2]==s[i-1] or p[j-2]==".") and dp[i-1][j]==True:
+                        dp[i][j]=True
+                    else:
+                        dp[i][j]=False
+                else:
+                    dp[i][j]=False
+        return dp[len(s)][len(p)]
